9557baec7f5fc97682af2328ecde8aedec7a25ac
MarshalTito7 <parijat.niyogy7@gmail.com>
2022-07-14 01:19:34 +0530

    second commit

---
+++ routes/stores.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const {getStores} = require('../controllers/storeController')
+const router = express.Router();
+
+router.route('/').get(getStores)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,26 @@ const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const connectDB = require('./config/db')
 
 // load environment variables
 dotenv.config({path: './config/config.env'});
 
+// Connect to database
+connectDB();
+
 const app = express();
+
+// Body parser middleware
+app.use(express.json())
+
+// Enable cors
+app.use(cors())
+
+// Routes
+app.use('/api/v1/stores',require('./routes/stores'))
+
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => console.log(`Server running in ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
