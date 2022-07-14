d7d90d5412bc4dd97b5503e2f15f4148155be86d
MarshalTito7 <parijat.niyogy7@gmail.com>
2022-07-14 21:01:46 +0530

    models made

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,19 @@
-exports.getStores = (req,res,next) => {
-    res.send('Hello');
+const Store = require('../models/Store')
+
+exports.getStores = async (req,res,next) => {
+    try{
+        const stores = await Store.find();
+
+        return res.status(200).json({
+            success: true,
+            count: stores.length,
+            data: stores
+        });
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({
+            error: 'Server error'
+        });
+    }
 };
